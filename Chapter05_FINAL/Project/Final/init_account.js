eb8ba6e8b17a056c43a128fbf613bd8412c98773
tuantotti <tuantotti14032001@gmail.com>
2023-01-11 13:26:49 +0700

    fix

--- Chapter05_FINAL/Project/Final/init_account.js
+++ Chapter05_FINAL/Project/Final/init_account.js
@@ -1,19 +1,19 @@
 var Web3 = require("web3");
 
 web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
-console.log(typeof web3.eth.accounts[0]);
-// web3.eth.sendTransaction(
-//   {
-//     from: web3.eth.accounts[0],
-//     to: "0x682aa520e2a15a53e27649d92687fe181ce1ed17",
-//     value: "",
-//   },
-//   function (error, hash) {
-//     if (error) {
-//       console.log(error);
-//     }
-//     if (hash) {
-//       console.log(hash);
-//     }
-//   }
-// );
+// console.log( web3.eth.accounts[0]);
+web3.eth.sendTransaction(
+  {
+    from: web3.eth.accounts[0],
+    to: "0xa80db023ab5072e28407292654ef8a669e96e756",
+    value: "3000000000000000000",
+  },
+  function (error, hash) {
+    if (error) {
+      console.log(error);
+    }
+    if (hash) {
+      console.log(hash);
+    }
+  }
+);
